7589f72bcd5b1fdc43da940cafd552f2853b47ff
Naeem Khedarun <naeem.khedarun@sonovate.com>
2021-06-23 17:33:52 +0100

    Add azure pipelines checkout step

---
+++ source/Nuke.Common/CI/AzurePipelines/Configuration/AzurePipelinesCheckoutStep.cs
@@ -0,0 +1,34 @@
+﻿using JetBrains.Annotations;
+using Nuke.Common.Utilities;
+
+namespace Nuke.Common.CI.AzurePipelines.Configuration
+{
+    public enum AzurePipelinesCheckoutType
+    {
+        Self, None
+    }
+    
+    [PublicAPI]
+    public class AzurePipelinesCheckoutStep : AzurePipelinesStep
+    {
+        public AzurePipelinesCheckoutType CheckoutType { get; }
+        public bool Submodules { get; }
+
+        public AzurePipelinesCheckoutStep(AzurePipelinesCheckoutType checkoutType, bool submodules = false)
+        {
+            CheckoutType = checkoutType;
+            Submodules = submodules;
+        }
+
+        public override void Write(CustomFileWriter writer)
+        {
+            using (writer.WriteBlock($"- checkout: {CheckoutType.ToString().ToLower()}"))
+            {
+                if (Submodules)
+                {
+                    writer.WriteLine("submodules: true");
+                }
+            }
+        }
+    }
+}
